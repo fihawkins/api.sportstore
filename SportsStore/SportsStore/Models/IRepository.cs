0b96356dfac73ec29a7ef617a8f91fc912162764
fihawkins <filipe.ufabc@gmail.com>
2017-12-26 21:33:53 -0200

    repository

---
+++ SportsStore/SportsStore/Models/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public interface IRepository
+    {
+        IEnumerable<Product> Products { get; }
+        Task<int> SaveProductsAsync(Product product);
+        Task<Product> DeleteProductAsync(int productID);
+
+        IEnumerable<Order> Orders { get; }
+        Task<int> SaveOrdersAsync(Order order);
+        Task<Order> DeleteOrderAsync(int orderID);
+    }
+}
