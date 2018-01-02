2104f7ef9a294631fa0557edb33defe66a564022
engageflopes <filipe.lopes@engage.bz>
2018-01-02 16:35:06 -0200

    authentication client controller

---
+++ SportsStore/SportsStore/scripts/storeCommonController.js
@@ -0,0 +1,18 @@
+﻿var authenticateUrl = "/authenticate"
+
+var authenticate = function (successCallback) {
+    sendRequest(authenticateUrl, "POST", {
+        "grant_type": "password",
+        "username": model.username(),
+        "password": model.password(),
+    }, function (data) {
+        model.authenticated(true);
+        setAjaxHeaders({
+            Authorization: "bearer " + data.access_token
+        });
+        //aparentemente o successCallback do if é a função acima
+        if (successCallback) {
+            successCallback();
+        }
+    })
+}
